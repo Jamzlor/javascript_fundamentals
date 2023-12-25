68a1294b6a30ca6043c3f0f241eeb2239efa6bbd
Jamzlor <jameslo.dev@gmail.com>
2023-12-26 01:58:19 +0800

    Create game funciton
    enclosed all needed function into game() and called the function 5 times to determine a best of five games winner

--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,73 @@
+let playerSelection;
+let computerSelection;
+let playerWins = 0;
+let computerWins = 0;
+
 function getComputerChoice (){
     let computerChoice = Math.floor(Math.random() * 3);
   if(computerChoice === 0){
-    console.log('rock')
-    return 'rock';
+    computerSelection = 'rock';
   } else if (computerChoice === 1){
-    console.log('paper')
-    return 'paper';
+    computerSelection = 'paper';
   } else if (computerChoice === 2){
-    console.log('scissor')
-    return 'scissor';
+    computerSelection = 'scissor';
   }
 };
 
 
-let playerChoice = prompt('Player chooses? rock / paper / scissor').toLowerCase();
 
+function getPlayerChoice() {
+    playerSelection = prompt('Player chooses? rock / paper / scissor').toLowerCase()
+};
 
 function playRound(playerChoice, computerChoice){
+    
     if(playerChoice === computerChoice){
         return 'Tied'
-    } else if(playerChoice === 'rock'){
+    } if(playerChoice === 'rock'){
         if(computerChoice === 'paper'){
+            computerWins++;
             return 'You lose!'
         } else {
+            playerWins++;
             return 'You win!'
         }
-    } else if(playerChoice === 'paper'){
+    } if(playerChoice === 'paper'){
         if(computerChoice === 'scissor'){
+            computerWins++;
             return 'You lose!'
         } else {
+            playerWins++;
             return 'You win!'
         }
-    } else if(playerChoice === 'scissor'){
+    } if(playerChoice === 'scissor'){
         if(computerChoice === 'rock'){
+            computerWins++;
             return 'You lose!'
         } else {
+            playerWins++;
             return 'You win!'
         }
     }
 }
+function game(){
+    getComputerChoice();
+    getPlayerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log('Player Win: ' + playerWins);
+    console.log('Computer Win: ' + computerWins);
+}
+
+game();
+game();
+game();
+game();
+game();
+    
+
+
+
+
+
+
 
