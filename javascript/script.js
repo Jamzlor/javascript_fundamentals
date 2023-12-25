6bd7c6771ee03ccc7166e02ddb2bc27d128803f4
Jamzlor <jameslo.dev@gmail.com>
2023-12-25 19:28:02 +0800

    Remove computerChoice variable from global
    computerChoice is not longer in global and only within getComputerChoice()

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,13 @@
-let computerChoice;
-
 function getComputerChoice (){
-    computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
   if(computerChoice === 0){
-    console.log('rock', computerChoice)
+    console.log('rock')
     return 'rock';
   } else if (computerChoice === 1){
-    console.log('paper', computerChoice)
+    console.log('paper')
     return 'paper';
   } else if (computerChoice === 2){
-    console.log('scissor', computerChoice)
+    console.log('scissor')
     return 'scissor';
   }
 };
